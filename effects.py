6fa090b946a2f93a09a570550303efc1f1f459a4
Michjzuman <143267862+Michjzuman@users.noreply.github.com>
2026-02-15 16:42:28 +0100

    goofy aah speed effect

--- effects.py
+++ effects.py
@@ -1,16 +1,16 @@
 import arcade
 import math
+import random
 
 def draw_speed_lines(game):
+    rays = 60
     dis = 20
     rad = math.radians(game.player.dir)
     rad_plus_90 = math.radians(game.player.dir + 90)
     l = math.sqrt(
         game.player.speed_x ** 2 +
         game.player.speed_y ** 2
-    ) * 50 - 100 + 500
-    dis_x = math.sin(rad) * l
-    dis_y = math.cos(rad) * l
+    ) * 50 - 70
     color = arcade.color.WHITE
     lw = 2
     start = math.sqrt(
@@ -18,14 +18,18 @@ def draw_speed_lines(game):
         (game.height / 2) ** 2
     )
     
-    for i in range(5):
+    for i in range(rays):
+        dis_x = math.sin(rad) * (l * random.random())
+        dis_y = math.cos(rad) * (l * random.random())
         x = (
             game.width / 2 - math.sin(rad) * start +
-            (math.sin(rad_plus_90) * dis) * i
+            (math.sin(rad_plus_90) * dis) *
+            (i + 0.5 - rays / 2)
         )
         y = (
             game.height / 2 - math.cos(rad) * start +
-            (math.cos(rad_plus_90) * dis) * i
+            (math.cos(rad_plus_90) * dis) *
+            (i + 0.5 - rays / 2)
         )
         arcade.draw_line(
             x, y,
